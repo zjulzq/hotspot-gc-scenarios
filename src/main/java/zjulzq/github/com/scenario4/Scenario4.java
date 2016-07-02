0c8ca22a51e8d3739bf2969094b53fea280c99df
zjulzq <274249651@qq.com>
2016-07-02 16:14:58 +0800

    refs #9 scenario4: creating too many classes dynamically
    Signed-off-by: zjulzq <274249651@qq.com>

---
+++ src/main/java/zjulzq/github/com/scenario4/Scenario4.java
@@ -0,0 +1,36 @@
+package zjulzq.github.com.scenario4;
+
+import java.lang.reflect.Method;
+
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+
+/**
+ * Creating too many classes dynamically.
+ * -Xmx10m -Xms10m -XX:PrintGCDetails
+ */
+public class Scenario4 {
+
+    static class Tmp {
+
+    }
+
+    public static void main(String[] args) {
+        Tmp tmp = new Tmp();
+        while (!Thread.interrupted()) {
+            Enhancer enhancer = new Enhancer();
+            enhancer.setSuperclass(Tmp.class);
+            enhancer.setUseCache(false);
+            enhancer.setCallback(new MethodInterceptor() {
+                @Override
+                public Object intercept(Object arg0, Method arg1, Object[] arg2, MethodProxy arg3) throws Throwable {
+                    return arg3.invokeSuper(arg0, arg2);
+                }
+            });
+            enhancer.create();
+        }
+        System.out.println(tmp.hashCode());
+    }
+
+}
